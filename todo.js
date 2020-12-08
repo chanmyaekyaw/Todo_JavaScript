b4f17a20476c6282441d9cd0d6c70c7b723be28a
Chan Myae Kyaw <chanmyaekyawmay@gmail.com>
2020-12-08 16:26:29 +0630

    2nd commit

--- todo.js
+++ todo.js
@@ -16,7 +16,47 @@ removeButton.addEventListener("click", removeItem);
 var removeEveryButton = document.getElementById("removeEverything");
 removeEveryButton.addEventListener("click", removeEveryItem);
 
-function addItem() {}
+function addItem() {
+  var input = document.getElementById("input");
+  var item = input.value;
+  ul = document.getElementById("list");
+  var textnode = document.createTextNode(item);
+
+  var text = "Enter your todo";
+  var p = document.getElementById("p");
+  var alert = document.createTextNode(text);
+
+  if (item === "") {
+    p.appendChild(alert);
+    return false;
+  } else {
+    p.remove(text);
+
+    //create li
+    li = document.createElement("li");
+
+    //create checkbox
+    var checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.setAttribute("id", "check");
+
+    //create label
+    var label = document.createElement("label");
+    label.setAttribute("for", "item");
+
+    //add these elements on web page
+    // ul.appendChild(label);
+    li.appendChild(checkbox);
+    label.appendChild(textnode);
+    li.appendChild(label);
+    ul.insertBefore(li, ul.childNodes[0]);
+    setTimeout(() => {
+      li.className = "visual";
+    }, 2);
+
+    input.value = "";
+  }
+}
 
 function removeItem() {
   li = ul.children;
